64ee4876e885c65b1a0242c1aee174136f0b0bde
Oke.A <Okea.dev@gmail.com>
2024-06-21 16:28:36 +0100

    70% test coverage done

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -9,9 +9,22 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+     modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+
     }
 
     function testMinimumDollarIsFive() public view {
@@ -28,5 +41,31 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundMeFailWithoutEnoughEth() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+    function testFundUpdateFundedDataStructure() public funded{
+        
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded{
+        
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+       
+
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
     }
 }
